d8e130860ae00bf36efdfc072848088ab4699c44
Hongling Zhang <honglingzhang@Honglings-MacBook-Pro.local>
2022-03-05 16:24:46 -0700

    clean code using eslint

--- routes/customerSMS.js
+++ routes/customerSMS.js
@@ -15,32 +15,32 @@ module.exports = (db) => {
     JOIN users ON user_id = users.id
     GROUP BY users.name, users.phone`;
 
-   db.query(query)
+    db.query(query)
       .then((data) => {
         console.log("customer sms send");
         client.messages.create({
           body: `Hi ${data.rows[0].name}. Your order has been received! Your order will be ready in ${data.rows[0].sum} minutes!`,
           from: "+19377125923",
           to: `+1${data.rows[0].phone}`,
-        })
+        });
         setTimeout(function() {
           client.messages.create({
             body: 'Your order is completed! You can pick it up now!',
             from: "+19377125923",
             to: `+1${data.rows[0].phone}`,
-          })
-        }, 20 * 1000) // set fixed 30 seconds
+          });
+        }, 20 * 1000);// set fixed 30 seconds
 
         setTimeout(function() {
           let clearCart = `DELETE FROM cart_items
           WHERE user_id = 1`;
           db.query(clearCart)
             .then(data => {
-              console.log('items deleted', data)
-            })
-        }, 5 * 1000)
+              console.log('items deleted', data);
+            });
+        }, 5 * 1000);
 
-      }) // db query
+      }); // db query
   }); // router.post
   return router;
 }; // most out
